a0ad144c506a21e00268f98e4d22f3c494a44836
aykut-gumus <gumus.aykut@gmail.com>
2025-03-11 22:08:40 +0300

    Class, psvm, sout

--- src/main/java/com/aykutgumus/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/aykutgumus/_1_part_javatypes/_01_JavaTypes.java
@@ -1,4 +1,17 @@
 package com.aykutgumus._1_part_javatypes;
 
+// single comment
+/*
+dsdsd
+sdsadsa
+sdadad
+ */
+
 public class _01_JavaTypes {
-}
+    //psvm => tab
+    public static void main(String[] args) {
+        // Çıktı
+        //sout => tab
+        System.out.println("Karanlık arttıkça şafak yaklaşır.");
+    } // end psvm
+} //end class
