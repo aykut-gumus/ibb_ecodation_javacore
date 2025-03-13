7447a35a1cfd6359dccffa04b5dff9db4d3867cd
aykut-gumus <gumus.aykut@gmail.com>
2025-03-13 21:44:10 +0300

    Data variables, names rules

---
+++ src/main/java/com/aykutgumus/_1_part_javatypes/_04_Operators.java
@@ -0,0 +1,28 @@
+package com.aykutgumus._1_part_javatypes;
+
+public class _04_Operators {
+
+    public static void main(String[] args) {
+        // operator
+        // +, -, *, /
+        // %
+        // ++
+        // --
+        int number1=23, number2=7;
+        int addition = number1+number2;
+        int substraction = number1-number2;
+        int multiplication = number1*number2;
+        int division = number1/number2;
+        int modules = number1%number2;
+
+
+        System.out.println("Toplam: "+addition);
+        System.out.println("Çıkarma: "+substraction);
+        System.out.println("Çarpma: "+multiplication);
+        System.out.println("Bölme: "+division);
+        System.out.println("Kalan: "+modules);
+
+        // Mantıksal Operators: &&=VE ||=VEYA !=değil
+        // ++ 1 artır  -- 1 azalt
+    }
+}
